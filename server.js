1a952ff896ea5f8b619a68f6ef4b44cae85a0852
Sebastian Edwards <me@sebastianedwards.co.nz>
2020-04-15 16:41:44 +0800

    Initial commit.

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const ADMIN = '/admin.html';
+const PLAYING = '/playing.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .get('/admin', (req, res) => res.sendFile(ADMIN, { root: __dirname }))
+  .post('/admin', (req, res) => {
+    io.emit('play')
+    console.log('Playing song')
+    res.sendFile(PLAYING, { root: __dirname })
+  })
+  .get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
@@ -16,5 +24,3 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
